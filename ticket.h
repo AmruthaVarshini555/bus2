5cae2e7f989aeac19c9c777d46fb2bd8cfbe7a03
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-14 23:06:18 +0530

    Create ticket.h

---
+++ ticket.h
@@ -0,0 +1,39 @@
+#ifndef __TICKET_H__
+#define __TICKET_H__
+#include<iostream>
+#include"header.h"
+#include"logger.h"
+class Ticket
+{
+    private:
+        char name[BUFFER_SIZE], pnrNo[BUFFER_SIZE],date[BUFFER_SIZE];
+        Bus bus;
+    public:
+        Ticket(){
+            LOG_INFO("Default constructor of ticket");
+        }
+        void generateTicket(char*,Bus);
+        void bookTicket();
+        void editTicket();
+        void cancelTicket();
+        void displayTicket();
+        void showAllTickets();
+        
+        void showTicketsByPNR();
+        void showTicketsByName();
+        void showTicketsByBus();
+        void showTicketsBySource();
+        void showTicketsByDestination();
+        //getters
+        char *getName();
+        char *getPnrNo();
+        char *getDate();
+        //setters
+        void setName(char*);
+        //destructor
+        ~Ticket(){
+            LOG_INFO("Destructor of ticket");
+         }
+};
+
+#endif //__TICKET_H__
